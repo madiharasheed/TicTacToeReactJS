ba300fa74c957c0fd569aaf98d522fda748c7964
madiharasheed <madiha1512@gmail.com>
2021-03-18 15:10:10 -0400

    Add files via upload

---
+++ src/components/info/info.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+import "./info.css";
+
+class Info extends Component {
+  render() {
+    return <header>{this.props.info}</header>;
+  }
+}
+export default Info;
