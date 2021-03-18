f9bc0017ceea7ee96aeedf6f1fb35c27b8d18ce8
learn <learn@LAPTOP-ASDFSLDF>
2021-03-18 19:30:33 +0530

    Updated Info Spec

--- src/components/info/info.spec.jsx
+++ src/components/info/info.spec.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { shallow } from "enzyme";
 
 import Info from "./info";
-
+/*Testing a component as unit */
 describe("Info Component", () => {
   it("should render Info component", () => {
     const infoWrapper = shallow(<Info />);
