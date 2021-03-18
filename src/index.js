038db35c8a77e3f3dda8f017375917151dd0f514
madiharasheed <madiha1512@gmail.com>
2021-03-18 14:38:01 -0400

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
+
+import App from "./app";
+import "./index.css";
+import "./index.css";
+/*Initial phase to render */
+ReactDOM.render(<App />, document.getElementById("root"));
