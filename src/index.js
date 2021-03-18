964194c676b2c22feb22cb6e43ab8ffe075414e1
learn <learn@LAPTOP-ASDFSLDF>
2021-03-18 19:32:18 +0530

    Updated Index

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./app";
 import "./index.css";
 import "./index.css";
-
+/*Initial phase to render */
 ReactDOM.render(<App />, document.getElementById("root"));
