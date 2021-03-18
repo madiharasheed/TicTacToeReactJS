8198cc26e609db614f51c2af0e7b03b71619d35e
Madiha Rasheed <madiha1512@gmail.com>
2021-03-18 15:38:20 -0400

    Update Index

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import "bootstrap/dist/css/bootstrap.css";
 import App from "./app";
 import "./index.css";
 import "./index.css";
-/*Initial phase to render */
+/*Initial phase to the render */
 ReactDOM.render(<App />, document.getElementById("root"));
